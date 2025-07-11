149395dd7d884d6e22f9afa44c275fae77ed2a8f
Ruban2907 <rubanahmed29@gmail.com>
2025-07-11 19:51:38 +0500

    lecture 06

---
+++ 06/script.js
@@ -0,0 +1,25 @@
+console.log("hiiiii")
+//looooops
+//for,while,dowhile
+
+
+
+//for->when u know how many times 4u have to loop
+//while->when we dont know how many times the code will be run\
+//do-while->k code runs first then the codition is checked
+
+
+
+for(let i=0;i<11;i++)
+{
+    console.log("rUBan " + i);
+}
+let n=0
+let key=24
+while(n!=key)
+{
+    console.log(n=n+1);
+}
+do{
+    console.log(n+=1);
+}while(n!=key)
